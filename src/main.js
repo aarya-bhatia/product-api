5ec541be309aa70dd12d26c8ad2ad7e51d4a973e
Aarya Bhatia <aaryabhatia@Aaryas-MacBook-Air.local>
2020-07-24 22:35:09 +0530

    Inital commit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$api = "https://social-app-1-dbb91.web.app/api/"
+
 new Vue({
+  beforeCreate(){
+    console.log(this.$api)
+  },
   render: h => h(App),
 }).$mount('#app')
